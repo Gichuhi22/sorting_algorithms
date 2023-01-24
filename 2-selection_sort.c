99be05def1798fbe00b50d783e6acfe5e017f347
Gichuhi22 <bernardgichuhiwamwea@gmail.com>
2023-01-24 21:13:37 +0000

    modified

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,12 +1,20 @@
 #include "sort.h"
-
+#include <stdbool.h>
+/**
+ *selection_sort - implements selection sort algorithm
+ *@array: array pointer
+ *@size: number of array elements
+ *
+ */
 
 void selection_sort(int *array, size_t size)
 {
 	size_t i, min_idx, j, temp;
+	bool swapped = false;
 
-	i = 0;
-	j = 0;
+	if (!array)
+		return;
+
 	for (j = 0; j < size - 1; j++)
 	{
 		min_idx = j;
@@ -15,13 +23,15 @@ void selection_sort(int *array, size_t size)
 			if (array[i] < array[min_idx])
 			{
 				min_idx = i;
+				swapped = true;
 			}
 		}
-		if (min_idx != j)
+		if (swapped == true)
 		{
 			temp = array[min_idx];
 			array[min_idx] = array[j];
 			array[j] = temp;
+			swapped = false;
 		}
 		print_array(array, size);
 	}
